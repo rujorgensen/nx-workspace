2af488e2b3a8262c5bb409a121c8afa952da287a
rujorgensen <rujorgensen@gmail.com>
2022-07-22 22:14:50 +0200

    Added supertest

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -1,22 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
-
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 
 describe('AppController', () => {
-  let app: TestingModule;
+    let app: INestApplication;
+    let httpServer;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-  });
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule(
+            {
+                controllers: [
+                    AppController,
+                ],
+                providers: [
+                    AppService,
+                ],
+            }
+        ).compile();
 
-  describe('getData', () => {
-    it('should return "Welcome to api!"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
+        app = moduleRef.createNestApplication();
+
+        await app.init();
+
+        httpServer = app.getHttpServer();
     });
-  });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    describe('getData', () => {
+        it('should return "Welcome to api!"', () => {
+            const appController = app.get<AppController>(AppController);
+            expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
+        });
+    });
+
+    // ******************************************************************************
+    // *** GET
+    // ******************************************************************************
+
+    it(`/GET testing returned response`, () => {
+        return request(httpServer)
+            .get('/hello')
+            .expect(HttpStatus.OK)
+            .expect(
+                { message: 'Welcome to api!' },
+            )
+            ;
+    });
 });
