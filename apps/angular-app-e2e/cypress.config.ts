1b04f510cffd7076bed24cf066b5ef39896b0e70
rj <rujorgensen@gmail.com>
2022-09-15 15:38:27 +0200

    Fixes e2e test

--- apps/angular-app-e2e/cypress.config.ts
+++ apps/angular-app-e2e/cypress.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
-import setupNodeEvents from './src/plugins/index';
+import * as setupNodeEvents from './src/plugins/index';
 
 const cypressJsonConfig = {
     fileServerFolder: '.',
@@ -8,8 +8,11 @@ const cypressJsonConfig = {
     video: true,
     videosFolder: '../../dist/cypress/apps/angular-app-e2e/videos',
     screenshotsFolder: '../../dist/cypress/apps/angular-app-e2e/screenshots',
-    chromeWebSecurity: false,
-    specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: [
+        '**/*.{feature,features}',
+        '**/*.spec.ts',
+        'src/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    ],
     supportFile: 'src/support/e2e.ts',
 };
 export default defineConfig({
