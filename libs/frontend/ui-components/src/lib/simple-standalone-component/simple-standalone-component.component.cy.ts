b162f722a6d9b8d96240413a9a008b07d2be2738
rujorgensen <rujorgensen@gmail.com>
2022-10-06 20:04:32 +0200

    BU

--- libs/frontend/ui-components/src/lib/simple-standalone-component/simple-standalone-component.component.cy.ts
+++ libs/frontend/ui-components/src/lib/simple-standalone-component/simple-standalone-component.component.cy.ts
@@ -1,14 +1,28 @@
+/* import { CommonModule } from '@angular/common';
+import { StandalonePipe } from '@frontend/pipes'; */
 import { MountConfig, mount } from 'cypress/angular';
 import { SimpleStandaloneComponentComponent } from './simple-standalone-component.component';
 
 describe(SimpleStandaloneComponentComponent.name, () => {
     const config: MountConfig<SimpleStandaloneComponentComponent> = {
         declarations: [],
-        imports: [],
-        providers: []
+        imports: [
+
+        ],
+        providers: [
+            /*      CommonModule,
+                 StandalonePipe, */
+        ]
     }
 
     it('renders', () => {
         mount(SimpleStandaloneComponentComponent, config);
+    })
+
+    it('should contain pipe output', () => {
+        mount(SimpleStandaloneComponentComponent, config);
+        
+        cy.get('b')
+            .should('contain', 'standalone-pipe');
     })
 })
