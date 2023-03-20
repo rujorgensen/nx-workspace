3ac54096115c5408ec938c0580a869412dd32f7e
rj <rujorgensen@gmail.com>
2023-03-20 13:57:40 +0100

    Migrated to @badeball/cypress-cucumber-preprocessor

--- apps/angular-app-e2e/cypress.config.ts
+++ apps/angular-app-e2e/cypress.config.ts
@@ -1,6 +1,9 @@
+// Configuration from https://github.com/badeball/cypress-cucumber-preprocessor/blob/HEAD/docs/quick-start.md
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
-import * as setupNodeEvents from './src/plugins/index';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import * as createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 const cypressJsonConfig = {
     fileServerFolder: '.',
@@ -15,17 +18,28 @@ const cypressJsonConfig = {
     ],
     supportFile: 'src/support/e2e.ts',
 };
+async function setupNodeEvents(
+    on: Cypress.PluginEvents,
+    config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+    // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+    await addCucumberPreprocessorPlugin(on, config);
+
+    on(
+        "file:preprocessor",
+        createBundler({
+            plugins: [createEsbuildPlugin(config)],
+        })
+    );
+
+    // Make sure to return the config object as it might have been modified by the plugin.
+    return config;
+}
+
 export default defineConfig({
     e2e: {
         ...nxE2EPreset(__dirname),
         ...cypressJsonConfig,
         setupNodeEvents,
-        /**
-        * TODO(@nrwl/cypress): In Cypress v12,the testIsolation option is turned on by default. 
-        * This can cause tests to start breaking where not indended.
-        * You should consider enabling this once you verify tests do not depend on each other
-        * More Info: https://docs.cypress.io/guides/references/migration-guide#Test-Isolation
-        **/
-        testIsolation: false,
     },
 });
--- apps/angular-app-e2e/src/plugins/index.js
+++ apps/angular-app-e2e/src/plugins/index.js
@@ -1,12 +1,5 @@
-const browserify = require('@cypress/browserify-preprocessor');
-const cucumber = require('cypress-cucumber-preprocessor').default;
-const resolve = require('resolve');
+const { preprocessTypescript } = require('@cypress/webpack-preprocessor');
 
 module.exports = (on, config) => {
-    const options = {
-        ...browserify.defaultOptions,
-        typescript: resolve.sync('typescript', { baseDir: config.projectRoot }),
-    };
-
-    on('file:preprocessor', cucumber(options));
+    on('file:preprocessor', preprocessTypescript(config));
 };
