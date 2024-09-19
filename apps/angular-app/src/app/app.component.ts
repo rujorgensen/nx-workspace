58de3f886936c603b0cdc7853e2f4d0a7e23682e
rj <rj@oxyguard.dk>
2024-09-19 09:40:04 +0200

    Activate Angular template linter

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@happynrwl/api-interfaces';
 
+interface ISomeObject {
+    thisIsAlwaysThere: { str: string };
+}
+
 @Component({
     selector: 'happynrwl-root',
     templateUrl: './app.component.html',
@@ -9,6 +13,9 @@ import { Message } from '@happynrwl/api-interfaces';
 })
 export class AppComponent {
     hello$ = this.http.get<Message>('/api/hello');
+    protected someObject: ISomeObject = {
+        thisIsAlwaysThere: { str: 'Here I am' },
+    }
 
     constructor(
         private http: HttpClient,
